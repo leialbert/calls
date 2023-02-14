06664da00ec6e8b4f409a888cbd5db8ae0eaa468
Albert Lei <albert.lei@hotmail.com>
2023-02-14 19:21:36 +0800

    add logs model to record the request of call

--- logs/admin.py
+++ logs/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Log
+@admin.register(Log)
+class LogAdmin(admin.ModelAdmin):
+    search_fields = ['call_type','call_Id','caller','callee']
+    list_filter = (
+        ('created_at', admin.DateFieldListFilter),
+    )
+    date_hierarchy = 'created_at'
+    list_display = ['call_type','call_Id','caller','callee','created_at']
+    list_per_page = 30
--- logs/models.py
+++ logs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Log(models.Model):
+
+    class Meta:
+        verbose_name = "Log"
+        verbose_name_plural = "Logs"
+
+    # {"RewriteE164Req":{"callId":123137,"callerE164":"020cuishou","calleeE164":"3313941135255"}}
+    call_type = models.CharField('请求类型',max_length=8)
+    call_Id = models.IntegerField('呼叫ID')
+    caller = models.CharField('主叫号码',max_length=20)
+    callee = models.CharField('被叫号码',max_length=20)
+    created_at = models.DateTimeField('请求时间',auto_now_add=True)

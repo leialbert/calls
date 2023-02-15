55bac6e99b93309286e75cbf77dc3cb84d5c91d9
Albert Lei <albert.lei@hotmail.com>
2023-02-15 23:17:32 +0800

    change the datetime display format

--- logs/admin.py
+++ logs/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils import timezone
+
 
 # Register your models here.
 from .models import Log
@@ -10,5 +12,10 @@ class LogAdmin(admin.ModelAdmin):
     )
     # '%Y-%m-%d %H:%M:%S.%f'
     date_hierarchy = 'created_at'
-    list_display = ['call_type','call_Id','caller','callee','created_at']
+
+    list_display = ['call_type','call_Id','caller','callee','created_at_formatted']
+    def created_at_formatted(self,obj):
+        return timezone.localtime(obj.created_at).strftime('%Y-%m-%d %H:%M:%S.%f')
+    created_at_formatted.short_description = 'Created at'
+
     list_per_page = 30

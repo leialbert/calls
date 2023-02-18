8260d836ab177e7873d3d3ad2b1db5851cf5806b
Albert Lei <albert.lei@hotmail.com>
2023-02-19 03:56:33 +0800

    gunicorn conf

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,7 +1,8 @@
 bind = "127.0.0.1:8000"
 workers = 3
+threads = 2
 timeout = 30
 keepalive = 2
-errorlog = "/var/log/gunicorn/error.log"
-accesslog = "/var/log/gunicorn/access.log"
+# errorlog = "/var/log/gunicorn/error.log"
+# accesslog = "/var/log/gunicorn/access.log"
 loglevel = "info"
